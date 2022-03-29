383711ed131637e23f1b97c2e42c6d4d76eab272
Augustus <54996373+Augustusgm@users.noreply.github.com>
2022-03-29 17:58:23 +0200

    Update GANtest.py

--- GANtest.py
+++ GANtest.py
@@ -280,12 +280,15 @@ for epoch in range(num_epochs):
         G_losses.append(errG.item())
         D_losses.append(errD.item())
 
-        # Check how the generator is doing by saving G's output on fixed_noise
-        if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
-            with torch.no_grad():
-                fake = netG(fixed_noise).detach().cpu()
-            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
+        # Output training stats
+#        if i % 390 == 0:
+#        print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+#                  % (epoch, num_epochs, i, len(dataloader),
+#                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+        print('[%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                  % (epoch, num_epochs,
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
         iters += 1
         
 plt.figure(figsize=(10,5))
