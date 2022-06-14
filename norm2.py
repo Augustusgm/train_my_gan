0aa93d4e16395649c596e187c3c17f0c34963e28
augustin <augustin.agm@gmail.com>
2022-06-14 20:09:23 +0200

    norm

--- norm2.py
+++ norm2.py
@@ -96,6 +96,7 @@ for k in range(len(zz)):
     z = zz[k]
     Vz = torch.randn(nbE, nz, 1, 1, device=device)
     Vz2 = Vz.clone().detach()
+    print(Vz2.size())
     for i in range(nbE):
         for j in range(nz):
             Vz2[i][j]= torch.sqrt((Vz2[i][j] - Cbackdoor_RED[j])*z**2/torch.sum(Vz2[i] - Cbackdoor_RED))
