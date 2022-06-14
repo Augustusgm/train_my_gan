29d9e054d6acb739464d5e550fbc13082a88ac2c
augustin <augustin.agm@gmail.com>
2022-06-14 20:33:35 +0200

    norm

--- norm2.py
+++ norm2.py
@@ -98,7 +98,7 @@ for k in range(len(zz)):
     Vz2 = Vz.clone().detach()
     for i in range(nbE):
         for j in range(nz):
-            Vz2[i][j]= np.sqrt((Vz2[i][j].cpu() - Cbackdoor_RED[j].cpu())*z**2/(torch.sum(Vz2[i] - Cbackdoor_RED[0]).cpu()))
+            Vz2[i][j]= np.sqrt((Vz2[i][j].cpu() - Cbackdoor_RED[j].cpu())*z**2/(torch.sum(Vz2[i] - Cbackdoor_RED[0]).cpu()[0]))
     
     Vz05 = Vz.clone().detach()
     for i in range(nbE):
